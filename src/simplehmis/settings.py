6969f97721a4135a399a8801a903bf896ae589fa
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-07-20 15:38:43 -0400

    Fix the dj_database_url configuration

--- src/simplehmis/settings.py
+++ src/simplehmis/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'simplehmis.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 import dj_database_url
-DATABASES = dj_database_url.config() or {
-    'default': {
+DATABASES = {
+    'default': dj_database_url.config() or {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
