12ed3f8c1eb44383eec61030cb7cc51342027e74
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-07-21 17:45:08 -0400

    Update the test data with more data

--- src/simplehmis/tests.py
+++ src/simplehmis/tests.py
@@ -23,3 +23,22 @@ class HMISUserTests (TestCase):
         project_admin = models.HMISUser(User.objects.get(username='project-admin1'))
         assert project_admin.is_project_staff()
         assert not intake_admin.is_project_staff()
+
+
+class EnrollmentFilterTests (TestCase):
+    fixtures = ['staff-groups.yaml', 'hmis-test-data.yaml']
+
+    def test_householdmember_pending_status(self):
+        pending_members = models.HouseholdMember.objects.filter_by_enrollment('-1')
+        assert len(pending_members) == 3
+        assert set(member.client.first for member in pending_members) == set(['Marisol', 'Rashad', 'Orlando'])
+
+    def test_householdmember_enrolled_status(self):
+        enrolled_members = models.HouseholdMember.objects.filter_by_enrollment('0')
+        assert len(enrolled_members) == 2
+        assert set(member.client.first for member in enrolled_members) == set(['Franklin', 'Magda'])
+
+    def test_householdmember_exited_status(self):
+        exited_members = models.HouseholdMember.objects.filter_by_enrollment('1')
+        assert len(exited_members) == 2
+        assert set(member.client.first for member in exited_members) == set(['Eunice', 'Dorris'])
