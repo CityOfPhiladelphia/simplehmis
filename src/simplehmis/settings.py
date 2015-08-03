ecf8acb34b178571920b05e5d28d21bfcabdf935
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-08-03 16:21:11 -0400

    Set allowed hosts in the settings

--- src/simplehmis/settings.py
+++ src/simplehmis/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'm69vy!#^7&@bjm2n(a8t@kezmg')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'True') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = (
+    os.environ.get('ALLOWED_HOSTS', '').split(',') or
+    ['*']
+)
 
 
 # Application definition
