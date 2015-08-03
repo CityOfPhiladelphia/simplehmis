8f24d6d916208264a1718a2a1e2e5e5bfbffa95f
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-08-03 11:18:34 -0400

    Add notes to the restart-server script

--- restart-server.sh
+++ restart-server.sh
@@ -2,7 +2,11 @@
 
 # Update the app supervisor configuration
 foreman export supervisord . --log /home/ubuntu/simplehmis/log --app simplehmis --user nobody
+# Explicitly run the server command with bash, so that
+# environment variables are properly replaced.
 sed -i 's/^command=\(.*\)$/command=bash -c "\1"/' simplehmis.conf
+# Move the generated config file into the supervisor conf
+# folder.
 sudo mv simplehmis.conf /etc/supervisor/conf.d/
 
 # Update the nginx configuration
