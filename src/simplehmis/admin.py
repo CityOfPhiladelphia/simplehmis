1da32f3cea6b6c044f1bb3f772b775c8e093c2a9
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-08-11 23:35:37 -0400

    Use the admin login view for authentication, so that we can correctly derive from the admin templates with the context in tact

--- src/simplehmis/admin.py
+++ src/simplehmis/admin.py
@@ -2,13 +2,35 @@ from django.contrib import admin
 from django.core.urlresolvers import reverse
 from django.db.models import fields
 from django.forms import widgets
+from django.shortcuts import render
 from django.utils.translation import ugettext as _
+from nopassword.forms import AuthenticationForm
+from nopassword.models import LoginCode
 from . import forms
 from . import models
 
 
 class AdminSite(admin.AdminSite):
     site_header = _('Philadelphia Simple HMIS')
+    login_form = AuthenticationForm
+
+    def login(self, request, extra_context=None):
+        if request.method == 'POST':
+            form = self.login_form(data=request.POST)
+            if form.is_valid():
+                code = LoginCode.objects.filter(user__username=request.POST.get('username'))[0]
+                code.next = request.GET.get('next')
+                code.save()
+                code.send_login_code(
+                    secure=request.is_secure(),
+                    host=request.get_host(),
+                )
+                return render(request, 'registration/sent_mail.html')
+
+        if request.method == 'GET':
+            request.session.set_test_cookie()
+
+        return super().login(request, extra_context=extra_context)
 
 
 class HouseholdMemberInline (admin.TabularInline):
