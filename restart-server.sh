b8490cd6b080f3ca414747b59aff5717c783db84
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-08-18 16:50:11 -0400

    Explicitly specify the folder where the simplehmis environment's python binary lives

--- restart-server.sh
+++ restart-server.sh
@@ -10,7 +10,7 @@ echo "    Updating the supervisor configuration..."
 foreman export supervisord . --log /srv/simplehmis/log --app simplehmis --user nobody
 # Explicitly run the server command with bash, so that
 # environment variables are properly replaced.
-sed -i 's/^command=\(.*\)$/command=bash -c "\1"/' simplehmis.conf
+sed -i 's/^command=\(.*\)$/command=bash -c "\/srv\/simplehmis\/bin\/\1"/' simplehmis.conf
 # Move the generated config file into the supervisor conf
 # folder.
 sudo mv simplehmis.conf /etc/supervisor/conf.d/
